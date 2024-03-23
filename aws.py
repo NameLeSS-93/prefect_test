0bea5bbb6acbb3b0f50c734643dbe7dda3fbf889
Nikita Myakishev <nmyakishev-93@yandex-team.ru>
2024-03-23 17:21:05 +0400

    fix aws command

--- aws.py
+++ aws.py
@@ -8,7 +8,7 @@ PATH = '/home/nmyakishev-93/openalex-snapshot-new'
 @task
 def sync_s3(path: str = PATH) -> 0:
     proc = subprocess.Popen(
-        f'cd {path} && aws s3 sync "s3://openalex" "openalex-snapshot" --no-sign-request --region "us-west-1"',
+        f'aws s3 sync "s3://openalex" "{path}" --no-sign-request --region "us-west-1"',
         stdout=subprocess.PIPE,
         bufsize=1,
         universal_newlines=True,
