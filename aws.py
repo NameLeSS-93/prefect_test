91b43ff0afe90ac1ef2932b13b14832793dc1070
Nikita Myakishev <nmyakishev-93@yandex-team.ru>
2024-03-23 17:26:11 +0400

    comma

--- aws.py
+++ aws.py
@@ -9,7 +9,7 @@ PATH = '/home/nmyakishev-93/openalex-snapshot-new'
 def sync_s3(path: str = PATH) -> 0:
     proc = subprocess.Popen(
         # f'aws s3 sync "s3://openalex" "{path}" --no-sign-request --region "us-west-1"',
-        'touch /home/nmyakishev-93/openalex-snapshot-new/test'
+        'touch /home/nmyakishev-93/openalex-snapshot-new/test',
         stdout=subprocess.PIPE,
         bufsize=1,
         universal_newlines=True,
